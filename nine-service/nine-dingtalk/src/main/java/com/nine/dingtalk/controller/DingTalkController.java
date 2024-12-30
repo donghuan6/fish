70eabe0c3059a7f5b033997e2855b6fe5c8f9f5d
donghuan <d_donghuan@126.com>
2024-12-30 18:00:09 +0800

    up

---
+++ nine-service/nine-dingtalk/src/main/java/com/nine/dingtalk/controller/DingTalkController.java
@@ -0,0 +1,15 @@
+package com.nine.dingtalk.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/dingtalk")
+@AllArgsConstructor
+public class DingTalkController {
+
+
+
+}
